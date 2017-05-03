b5ffc175d065d22783662fbc63f0faae207e24a3
Kyeongmin Noh <ppsseo18@gmail.com>
2017-05-04 01:02:08 +0900

    Make main.ejs main.css

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set("view engine", 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.render('main');
+});
+
+http.listen(3000, function(){
+    console.log('Server On!');
+});
