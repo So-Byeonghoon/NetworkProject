f1ac562208f13b489704b9cffe2062522db9bc12
Byounghoon So <sobh4728@gmail.com>
2017-05-12 17:16:52 +0900

    CHG: when show/make project in app.js, use myql

--- app.js
+++ app.js
@@ -27,68 +27,6 @@ app.set("view engine", 'ejs');
 
 app.use(express.static('public'));
 
-
-var projects = [
-    {
-        pid: 1,
-        name: 'posrello'
-    },
-    {
-        pid: 2,
-        name: 'network'
-    },
-    {
-        pid: 3,
-        name: 'trello'
-    }
-];
-var involve = [
-    {
-        pid: 1,
-        userid: 1
-    },
-    {
-        pid: 1,
-        userid: 2
-    },
-    {
-        pid: 1,
-        userid: 3
-    },
-    {
-        pid: 2,
-        userid: 1
-    },
-    {
-        pid: 2,
-        userid: 2
-    },
-    {
-        pid: 3,
-        userid: 1
-    },
-    {
-        pid: 3,
-        userid: 3
-    }
-];
-
-var substeps = [
-    {
-        sid: 1,
-        pid: 1,
-        name: 'make main page',
-        due: '20170509'
-    }
-]
-
-var involveSubsteps = [
-    {
-        sid: 1,
-        userid: 1
-    }
-]
-
 app.get('/', function(req,res){
     if(req.session.username){                   // if already logged-in, redirect to main
         console.log("can't acess login page");
@@ -119,13 +57,7 @@ app.get('/login', function(req,res){
     }
     SignIn(req, function (req) {
 		dbCon.query(sql.addUser(req.query.username), function(err, rows) {
-			dbCon.query(sql.setUser(req.query.username), function (err, rows) {
-		    	if (Object.keys(rows).length) {
-			    req.session.username = rows[0].name;
-			    req.session.userid = rows[0].userid;
-				res.redirect('/main');
-		    	}
-			});
+			SignIn(req, function(req) {});
 		});
 	});
 });
@@ -135,6 +67,7 @@ app.get('/main', function(req,res){
     if(typeof req.session.username === 'undefined' || !req.session.username){                  // if not logged in, redirect to login page
         console.log('need to login');
         res.redirect('/');
+        return;
     }
     var msg = '';
     if(req.session.msg){                        // check session have message to client
@@ -143,43 +76,17 @@ app.get('/main', function(req,res){
     }
     console.log('session user: ', req.session.username);
     console.log('session userid: ', req.session.userid);
-    var pidList = []; 
-   for(var i in involve){                       // find project that user involved
-        if(involve[i].userid == req.session.userid) {
-            pidList.push(involve[i].pid);
-        }
-    }
-    var projectList = projects.filter(function(value) { // find projectname that user involved
-        for(var i in pidList){
-            if(pidList[i] == value.pid){
-                return true;
-            }
-        }
-        return false;
-    });
 
-    var sidList = [];
-    for(var i in involveSubsteps){              // find substep that user involved
-        if(involveSubsteps[i].userid == req.session.userid){
-            sidList.push(involveSubsteps[i].sid);
-        }
-    }
-
-    var substepList = substeps.filter(function(value) { // find substepname that user involved
-        for(var i in sidList){
-            if(sidList[i] == value.sid){
-                return true
-            }
-        }
-        return false;
-    })
-    res.render('main', { projectList: projectList, substepList: substepList, msg: msg } );
-       
+    dbCon.query(sql.getProjectList(req.session.userid), function (err, projects) {
+    	dbCon.query(sql.getSubsteps(req.session.userid), function (err, substeps) {
+    		res.render('main', { projectList: projects, substepList: substeps, msg: msg } );
+		});
+	});
 });
 
 app.get('/logout', function(req, res){
     if(req.session.userid){                                 // if user logged-in, log out
-        req.session.destroy(function(err){                  
+        req.session.destroy(function(err){
             if(err){
                 console.log(err);
             }else{
@@ -193,22 +100,12 @@ app.get('/logout', function(req, res){
 
 app.get('/makeProject', function(req,res){
     if(req.query.projectname){
-        var check =false
-        for(var i in projects){                             // check that DB already have same name project
-            if(projects[i].name == req.query.projectname){
-                check = true;
-                            }
-        }
-        if(check){                                          // if already have same name project, set error message
-            req.session.msg ='same name';
-            res.redirect('/main');
-        }else{                                              // add project to DB
-            projects.push({ pid : projects.length + 1, name: req.query.projectname });
-            involve.push({ pid: projects.length, userid: req.session.userid });
-            res.redirect('/main');
-        }
-    }else{
-        res.redirect('/main');
+    	dbCon.query(sql.makeProject(req.query.projectname), function (err, okpacket) {
+    		var statement = sql.addProjectMember(okpacket.insertId, req.session.userid, '');
+    		dbCon.query(statement, function (err, okpacket) {
+		    	res.redirect('/main');
+    		});
+		});
     }
 })
 
--- db/SQL.js
+++ db/SQL.js
@@ -2,13 +2,41 @@ var SQL = function() {};
 
 SQL.prototype = {
 	setUser: function(name) {
-		this.name = name;
 		return 'SELECT userid, name FROM Users ' +
-			   'WHERE name ="' + this.name + '"';
+			   'WHERE name ="' + name + '"';
 	},
 	addUser: function(name) {
-		this.name = name;
-		return 'INSERT INTO Users (name) VALUE ("' + this.name + '")';
+		return 'INSERT INTO Users (name) VALUE ("' + name + '")';
+	},
+	getProjectList: function(userid) {
+		return 'SELECT P.pid, P.name ' +
+			   'FROM Projects P, Involves I ' +
+			   'WHERE P.pid = I.pid ' +
+			     'AND I.uid ="' + userid + '"';
+	},
+	makeProject: function(project_name) {
+		// Just make new Project, not add any member
+		return 'INSERT INTO Projects (name) VALUE ("' + project_name + '")';
+	},
+	addProjectMember: function(pid, userid, username) {
+		// pid MUST be required
+		// only 1 of userid or username is required
+		if (userid) {
+			return 'INSERT INTO Involves (pid, uid) ' +
+				   'VALUE ("' + pid + '", "' + userid + '")';
+		} else {
+			return 'INSERT INTO Involves (pid, uid) ' +
+				   'SELECT "' + pid + '", uid ' +
+				   'FROM Users ' +
+				   'WHERE name = "' + username + '"';
+		}
+	},
+	getSubsteps: function(userid) {	
+		return 'SELECT P.pid, S.sid, P.name project_name, S.name ' +
+			   'FROM Projects P, Substeps S, Involve_Substeps I ' +
+			   'WHERE P.pid = S.pid ' +
+			     'AND I.sid = S.sid ' +
+			     'AND I.uid ="' + userid + '"';
 	}
 };
 
